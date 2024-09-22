b29f753193e9eb475ad704390bdd342b9d1e58cb
come-collab <cometrisse640@gmail.com>
2024-09-22 11:37:52 +0200

    handled connection

--- src/jobs/streaming-socket.py
+++ src/jobs/streaming-socket.py
@@ -3,33 +3,36 @@ import socket
 import time
 import pandas as pd
 
-def send_data_over_socket(file_path, host='127.0.0.1', port=9999, chunk_size=2):
+def send_data_over_socket(file_path, host='127.0.0.1', port=9999, chunk_size=5):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
     s.listen(1)
     print("Server listening on port", port)
 
-    conn, addr = s.accept()
-    print(f"Connection from {addr}")
+  
 
-    try:
-        with open(file_path, 'rb') as file:
-            records = []
-            for line in file:
-                records.append(json.loads(line))
-                if len(records) == chunk_size:
-                    chunk = pd.DataFrame(records)
-                    print(chunk)
-                    for record in chunk.to_dict(orient='records'):
-                        serialize_data = json.dumps(record).encode('utf-8')
-                        conn.send(serialize_data + b'\n')
-                        time.sleep(1)  # Reduced sleep for faster testing
-                    records = []
-    except (BrokenPipeError, ConnectionResetError):
-        print("Client disconnected")
-    finally:
-        conn.close()
-        print("Connection closed")
+    while True:
+        conn, addr = s.accept()
+        print(f"Connection from {addr}")
+        
+        try:
+            with open(file_path, 'rb') as file:
+                records = []
+                for line in file:
+                    records.append(json.loads(line))
+                    if len(records) == chunk_size:
+                        chunk = pd.DataFrame(records)
+                        print(chunk)
+                        for record in chunk.to_dict(orient='records'):
+                            serialize_data = json.dumps(record).encode('utf-8')
+                            conn.send(serialize_data + b'\n')
+                            time.sleep(1)  # Reduced sleep for faster testing
+                        records = []
+        except (BrokenPipeError, ConnectionResetError):
+            print("Client disconnected")
+        finally:
+            conn.close()
+            print("Connection closed")
 
 
 if __name__ == "__main__":
