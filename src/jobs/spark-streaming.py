7ffbed9d32f4023e894b40d9f64ec79b1023faea
come-collab <cometrisse640@gmail.com>
2024-09-22 11:20:24 +0200

    its working

--- src/jobs/spark-streaming.py
+++ src/jobs/spark-streaming.py
@@ -1,5 +1,12 @@
+import pyspark
 from pyspark.sql import SparkSession
+
+def start_streaming(spark):
+   stream_df = spark.readStream.format("socket").option("host", "127.0.0.1").option("port", 9999).load()
+   query = stream_df.writeStream.outputMode("append").format("console").start()
+   query.awaitTermination()
 
 
 if __name__ == "__main__":
-    spark_conn = SparkSession.builder.appName("SparkStreaming").getOrCreate()
+    spark = SparkSession.builder.appName("SocketStreamConsumer").getOrCreate()
+    start_streaming(spark)
