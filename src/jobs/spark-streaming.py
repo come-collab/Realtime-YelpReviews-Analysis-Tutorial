ba9b97f53fe6253bda38d23a2a2d8513ab758078
come-collab <cometrisse640@gmail.com>
2024-09-23 08:20:59 +0200

    finished the project

--- src/jobs/spark-streaming.py
+++ src/jobs/spark-streaming.py
@@ -1,29 +1,64 @@
-import pyspark
-from pyspark.sql import SparkSession
-from pyspark.sql.types import StructField, StructType, StringType,FloatType
-from pyspark.sql.functions import from_json, col, to_json, struct
-from config.config import config
-from confluent_kafka import Producer, Consumer
-import time 
+from time import sleep
 
+import pyspark
+import openai
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col, when, udf
+from pyspark.sql.types import StructType, StructField, StringType, FloatType
+from config.config import config
+
+def sentiment_analysis(comment) -> str:
+    if comment:
+        openai.api_key = config['openai']['api_key']
+        completion = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages = [
+                {
+                    "role": "system",
+                    "content": """
+                        You're a machine learning model with a task of classifying comments into POSITIVE, NEGATIVE, NEUTRAL.
+                        You are to respond with one word from the option specified above, do not add anything else.
+                        Here is the comment:
+                        
+                        {comment}
+                    """.format(comment=comment)
+                }
+            ]
+        )
+        return completion.choices[0].message['content']
+    return "Empty"
 
 def start_streaming(spark):
-   topic = 'Review'
-   try : 
-      stream_df = spark.readStream.format("socket").option("host", "127.0.0.1").option("port", 9999).load()
-      schema = StructType ([
-         StructField("review_id",StringType()),
-         StructField("user_id",StringType()),
-         StructField("business_id", StringType()),
-         StructField("stars",FloatType()),
-         StructField("date",StringType()),
-         StructField("text",StringType())
-      ])
+    topic = 'customers_review'
+    while True:
+        try:
+            stream_df = (spark.readStream.format("socket")
+                         .option("host", "0.0.0.0")
+                         .option("port", 9999)
+                         .load()
+                         )
 
-      stream_df = stream_df.select(from_json(col('value'), schema).alias("data")).select(("data.*"))
-      
-      kafka_df = stream_df.selectExpr("CAST(review_id AS STRING) AS key","to_json(struct(*)) AS value")
-      query = (kafka_df.writeStream
+            schema = StructType([
+                StructField("review_id", StringType()),
+                StructField("user_id", StringType()),
+                StructField("business_id", StringType()),
+                StructField("stars", FloatType()),
+                StructField("date", StringType()),
+                StructField("text", StringType())
+            ])
+
+            stream_df = stream_df.select(from_json(col('value'), schema).alias("data")).select(("data.*"))
+
+            sentiment_analysis_udf = udf(sentiment_analysis, StringType())
+
+            stream_df = stream_df.withColumn('feedback',
+                                             when(col('text').isNotNull(), sentiment_analysis_udf(col('text')))
+                                             .otherwise(None)
+                                             )
+
+            kafka_df = stream_df.selectExpr("CAST(review_id AS STRING) AS key", "to_json(struct(*)) AS value")
+
+            query = (kafka_df.writeStream
                    .format("kafka")
                    .option("kafka.bootstrap.servers", config['kafka']['bootstrap.servers'])
                    .option("kafka.security.protocol", config['kafka']['security.protocol'])
@@ -39,12 +74,12 @@ def start_streaming(spark):
                    .start()
                    .awaitTermination()
                 )
-      
-   except Exception as e :
-      print(f'Exception encountedred : {e}. Retrying in 10 seconds')
-      time.sleep(10)
 
+        except Exception as e:
+            print(f'Exception encountered: {e}. Retrying in 10 seconds')
+            sleep(10)
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("SocketStreamConsumer").getOrCreate()
-    start_streaming(spark)
+    spark_conn = SparkSession.builder.appName("SocketStreamConsumer").getOrCreate()
+
+    start_streaming(spark_conn)
--- src/jobs/streaming-socket.py
+++ src/jobs/streaming-socket.py
@@ -3,39 +3,45 @@ import socket
 import time
 import pandas as pd
 
-def send_data_over_socket(file_path, host='127.0.0.1', port=9999, chunk_size=2):
+def handle_date(obj):
+    if isinstance(obj, pd.Timestamp):
+        return obj.strftime('%Y-%m-%d %H:%M:%S')
+    raise TypeError("Object of type '%s' is not JSON serializable" % type(obj).__name__)
+
+def send_data_over_socket(file_path, host='spark-master', port=9999, chunk_size=2):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
     s.listen(1)
-    print("Server listening on port", port)
+    print(f"Listening for connections on {host}:{port}")
 
-  
-
+    last_sent_index = 0
     while True:
         conn, addr = s.accept()
         print(f"Connection from {addr}")
-
         try:
-            with open(file_path, 'rb') as file:
+            with open(file_path, 'r') as file:
+                # skip the lines that were already sent
+                for _ in range(last_sent_index):
+                    next(file)
+
                 records = []
                 for line in file:
                     records.append(json.loads(line))
-                    if len(records) == chunk_size:
+                    if(len(records)) == chunk_size:
                         chunk = pd.DataFrame(records)
                         print(chunk)
                         for record in chunk.to_dict(orient='records'):
-                            serialize_data = json.dumps(record).encode('utf-8')
+                            serialize_data = json.dumps(record, default=handle_date).encode('utf-8')
                             conn.send(serialize_data + b'\n')
-                            time.sleep(5)  # Reduced sleep for faster testing
+                            time.sleep(5)
+                            last_sent_index += 1
+
                         records = []
         except (BrokenPipeError, ConnectionResetError):
-            print("Client disconnected")
+            print("Client disconnected.")
         finally:
             conn.close()
             print("Connection closed")
 
-
 if __name__ == "__main__":
-    send_data_over_socket('src/datasets/yelp_academic_dataset_review.json')
-
-
+    send_data_over_socket("datasets/yelp_academic_dataset_review.json")
