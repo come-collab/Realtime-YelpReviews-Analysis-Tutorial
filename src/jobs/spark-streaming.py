a21ed70af4f12fdf75c42cb4c8b1b335125e6515
come-collab <cometrisse640@gmail.com>
2024-09-23 07:52:51 +0200

    trying to connectto confluent cloud

--- src/jobs/spark-streaming.py
+++ src/jobs/spark-streaming.py
@@ -1,9 +1,12 @@
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, StringType,FloatType
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, to_json, struct
+from config.config import config
+from confluent_kafka import Producer, Consumer
 
 def start_streaming(spark):
+   topic = 'Review'
    try : 
       stream_df = spark.readStream.format("socket").option("host", "127.0.0.1").option("port", 9999).load()
       schema = StructType ([
@@ -16,10 +19,25 @@ def start_streaming(spark):
       ])
 
       stream_df = stream_df.select(from_json(col('value'), schema).alias("data")).select(("data.*"))
-
-      query = stream_df.writeStream.outputMode("append").format("console").options(truncate=False).start()
-      query.awaitTermination()
-
+      
+      kafka_df = stream_df.selectExpr("CAST(review_id AS STRING) AS key","to_json(struct(*)) AS value")
+      query = (kafka_df.writeStream
+                   .format("kafka")
+                   .option("kafka.bootstrap.servers", config['kafka']['bootstrap.servers'])
+                   .option("kafka.security.protocol", config['kafka']['security.protocol'])
+                   .option('kafka.sasl.mechanism', config['kafka']['sasl.mechanisms'])
+                   .option('kafka.sasl.jaas.config',
+                           'org.apache.kafka.common.security.plain.PlainLoginModule required username="{username}" '
+                           'password="{password}";'.format(
+                               username=config['kafka']['sasl.username'],
+                               password=config['kafka']['sasl.password']
+                           ))
+                   .option('checkpointLocation', '/tmp/checkpoint')
+                   .option('topic', topic)
+                   .start()
+                   .awaitTermination()
+                )
+      
    except Exception as e :
       print(e)
 
